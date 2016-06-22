ed6127eecfac93ecc1b0f21340c99210060bf18c
Carlos A. Cabrera <ccabrera@sapient.com>
2016-06-21 20:43:03 -0400

    Initial “ActionController” and model for User

---
+++ app/controllers/users/index.js
@@ -0,0 +1,26 @@
+const Controller = require('../../lib/controller');
+const controller = new Controller(require('../../models/user'));
+
+module.exports = class UserController {
+
+    static index(req, res, next) {
+        return controller.index(req, res, next);
+    }
+
+    static create(req, res, next) {
+        return controller.create(req, res, next);
+    }
+
+    static show(req, res, next) {
+        return controller.show(req, res, next);
+    }
+
+    static update(req, res, next) {
+        return controller.update(req, res, next);
+    }
+
+    static delete(req, res, next) {
+        return controller.delete(req, res, next);
+    }
+
+}
